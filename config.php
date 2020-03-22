9a865ed5410fbab53741f6948fb4600774295aec
bidlos <bad851@mail.ru>
2020-03-22 14:42:39 +0300

    New

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$host = 'localhost'; // адрес сервера 
-$database = 'user_shop'; // имя базы данных
-$user = 'root'; // имя пользователя
-$password = ''; // пароль
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-require_once 'function.php'; 
-include 'head.php';
-include 'body.php';
-include 'footer.php';
